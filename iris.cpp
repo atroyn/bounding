717765a3a386526b4b0cdd678c512e3916557a9a
Anton Troynikov <anton.troynikov@gmail.com>
2016-08-05 11:50:30 +0200

    Added infrastructure for reading coords etc.

--- iris.cpp
+++ iris.cpp
@@ -5,9 +5,30 @@
 #include <fstream>
 #include <sstream>
 
+bool getTarget(cv::Point &point, std::ifstream &coords) {
+
+    if(!coords.is_open()) {
+        return false;
+    }
+
+    std::string line;
+    if(!std::getline(coords, line)) {
+        return false;
+    }
+    std::istringstream sstream(line);
+
+    double x, y;
+
+    sstream >> x;
+    sstream >> y;
+
+    point = cv::Point(x,y);
+    return true;
+}
+
 int main(int argc, char** argv)
 {
-    // Assume correctness of args etc.
+    // Assume correctness of args.
     cv::VideoCapture cap(argv[1]);
     if(!cap.isOpened()) {
         return -1;
@@ -19,21 +40,14 @@ int main(int argc, char** argv)
     }
 
     cv::namedWindow("frame",1);
-    for(;;)
+    cv::Point target;
+
+    while(getTarget(target, coords))
     {
         cv::Mat frame;
         cap >> frame;
 
-        std::string line;
-        std::getline(coords, line);
-        std::istringstream sstream(line);
-
-        double x, y;
-
-        sstream >> x;
-        sstream >> y;
-
-        cv::circle(frame, cv::Point(x,y),10,cv::Scalar(0,255,0),2);
+        cv::circle(frame, target,10,cv::Scalar(0,255,0),2);
         imshow("frame", frame);
 
         cv::waitKey();
